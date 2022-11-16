2bce4b25190878b28432fa3b58c23ff57340e167
Jakub <jakub.bchyla@gmail.com>
2022-11-16 07:00:26 +0100

    task tracker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-crash-course-Travery-media';
+  title: string = 'Task tracker';
 }
